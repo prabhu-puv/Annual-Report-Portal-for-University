0ddf64a3253f3ead86a2f492346850189608d877
Prabhu-2004-04 <prabhanjanuv20040404@gmail.com>
2026-04-14 20:29:37 +0530

    final netlify fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,25 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   base: "/",
-
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-
-  plugins: [
-    react(),
-    mode === "development" && componentTagger()
-  ].filter(Boolean),
+  plugins: [react(), componentTagger()].filter(Boolean),
 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});

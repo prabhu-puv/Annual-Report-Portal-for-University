5df81708c9782cd0f0f184983c6e20e431f00039
Prabhu-2004-04 <prabhanjanuv20040404@gmail.com>
2026-04-14 15:44:14 +0530

    Update repository URL and site base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/CAPSTONE/",   // ← ADD THIS LINE
+  base: "/Annual-Report-Portal-for-University/",
 
   server: {
     host: "::",
